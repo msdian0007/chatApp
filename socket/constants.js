ef7ff56f6c9a166a784c7804505d1e967b8b4b34
mohasin attar <mohasin.attar@techo2.com>
2024-07-22 17:12:23 +0530

    socket env file fix

--- socket/constants.js
+++ socket/constants.js
@@ -4,8 +4,8 @@ if (process.env.stage === "local") {
   url = "http://localhost:5173/";
 } else if (process.env.stage === "local-ip") {
   url = "http://192.168.192.183:5173/";
-} else {
-  url = "";
+} else if(process.env.stage === "prod"){
+  url = process.env.baseUrl;
 }
 const baseUrl = Object.freeze({
   url,

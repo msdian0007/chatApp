1ead29532579530a626b306fc815fd0f1d401c7b
Mohasin Attar <111340539+msdian0007@users.noreply.github.com>
2024-07-25 22:45:53 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,12 +10,7 @@ const app = express();
 dotenv.config();
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(cookieParser());
 
 app.use("/api/user", userRoutes);
